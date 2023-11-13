d6cf659ccdb50c4cb26785b955cda940856b5bdd
Amirhoseinjkar <150539608+Amirhoseinjkar@users.noreply.github.com>
2023-11-13 18:20:55 +0330

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const container = document.querySelector (".container");
+const searchIcons = document.querySelectorAll(".search-bar i");
+
+searchIcons.forEach((searchIcon) => {
+  searchIcon.addEventListener("click", ()=> {
+    container.classList.toggle("change")
+   
+  });
+  
+});
+
